eb4c2fea0f17f41a033964507b83d7e875eb3606
Srutip04 <subuhishruti75@gmail.com>
2023-04-15 01:41:48 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import { Cart, Main } from "./pages";
+import Header from "./components/Header/Header";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link class="navbar-brand" to={"/"}>
+          Pizzaeria
+        </Link>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <Link class="nav-link" to={"/"}>
+                HOME
+              </Link>
+            </li>
+          </ul>
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <Link class="nav-link" to={"/cart"}>
+                CART 
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
